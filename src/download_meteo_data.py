1e843bb7a597c6bedc9d1662d1486f108b60f5a4
Przemek Garasz <pgdv@protonmail.com>
2021-11-14 18:49:56 +0100

    feat: overwrite or skip if date exists in db

--- src/download_meteo_data.py
+++ src/download_meteo_data.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select, update
 from sqlalchemy.orm import Session
 
 from common.utils import get_api_key, dates_generator, print_start_info
@@ -14,6 +14,7 @@ start_date = '2017-09-06'
 end_date = '2017-09-11'
 outpost_code = 31
 params = ['rain', 'winddir', 'windlevel', 'temp', 'pressure', 'humidity']
+overwrite = False
 
 gda_meteo = GdaMeteo(api_key)
 table = build_outpost_table(outpost_code)
@@ -23,16 +24,29 @@ table.__table__.create(bind=engine, checkfirst=True)
 
 print_start_info(outpost_code, start_date, end_date)
 
-with Session(engine) as session:
+with Session(engine, future=True) as session:
+
+    existing_dates = session.execute(select(table.datetime)).scalars().all()
 
     for date in dates_generator(start_date, end_date):
+
+        if not overwrite and datetime.fromisoformat(date) in existing_dates:
+            print(f'   {date} - already in database')
+            continue
 
         print(f'   {date}')
 
         hourly_data = gda_meteo.get_meteo_params(date, outpost_code, params)
 
         for dt in hourly_data:
-            row = table(datetime=datetime.fromisoformat(dt), **hourly_data[dt])
-            session.add(row)
+
+            if overwrite and datetime.fromisoformat(dt) in existing_dates:
+                stmt = update(table)                                                \
+                    .where(table.datetime == dt)                                    \
+                    .values(**hourly_data[dt])
+                session.execute(stmt)
+            else:
+                row = table(datetime=datetime.fromisoformat(dt), **hourly_data[dt])
+                session.add(row)
 
         session.commit()
