efbcebe2f9b09a4f5fa45c81c8d4dfe3f708bc9e
Przemek Garasz <pgdv@protonmail.com>
2021-12-28 16:27:42 +0100

    feat(stats): wind rose

--- src/db/statistics.py
+++ src/db/statistics.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select, func
+from sqlalchemy import select, func, case, cast, Float
 
 
 def monthly_avg_rainfall(outpost):
@@ -35,3 +35,64 @@ def monthly_avg_temp(outpost):
             func.strftime('%m', outpost.datetime))                  \
 
     return query
+
+
+def wind_rose(outpost):
+
+    angle = outpost.winddir
+    lvl = outpost.windlevel
+
+    whens_angle = {
+        ((angle >= 0) & (angle < 11.25)): 'N',
+        ((angle >= 11.25) & (angle < 33.75)): 'NNE',
+        ((angle >= 33.75) & (angle < 56.25)): 'NE',
+        ((angle >= 56.25) & (angle < 78.75)): 'ENE',
+        ((angle >= 78.75) & (angle < 101.25)): 'E',
+        ((angle >= 101.25) & (angle < 123.75)): 'ESE',
+        ((angle >= 123.75) & (angle < 146.25)): 'SE',
+        ((angle >= 146.25) & (angle < 168.75)): 'SSE',
+        ((angle >= 168.75) & (angle < 191.25)): 'S',
+        ((angle >= 191.25) & (angle < 213.75)): 'SSW',
+        ((angle >= 213.75) & (angle < 236.25)): 'SW',
+        ((angle >= 236.25) & (angle < 258.75)): 'WSW',
+        ((angle >= 258.75) & (angle < 281.25)): 'W',
+        ((angle >= 281.25) & (angle < 303.75)): 'WNW',
+        ((angle >= 303.75) & (angle < 326.25)): 'NW',
+        ((angle >= 326.25) & (angle < 348.75)): 'NNW',
+        ((angle >= 348.75) & (angle <= 360)): 'N'
+    }
+
+    whens_lvl = {
+        (lvl == 0): "c",
+        ((lvl > 0) & (lvl < 1)): "0-1",
+        ((lvl >= 1) & (lvl < 3)): "1-3",
+        ((lvl >= 3) & (lvl < 5)): "3-5",
+        ((lvl >= 5) & (lvl < 8)): "5-8",
+        ((lvl >= 8) & (lvl < 11)): "8-11",
+        ((lvl >= 11) & (lvl < 18)): "11-18",
+        ((lvl >= 18) & (lvl < 30)): "18-30",
+        (lvl >= 30): ">=30",
+    }
+
+    total =                                                          \
+        select(
+            cast(func.count(lvl), Float).label("total"))             \
+        .where(angle != None)                                        \
+        .subquery()
+
+    query =                                                          \
+        select(
+            case(whens_angle, else_=None).label("direction"),
+            case(whens_lvl, else_=None).label("strength"),
+            func.count().label("count"),
+            cast(func.count() / total * 100, Float)
+                .label("frequency"))                                \
+        .where(angle != None)                                       \
+        .group_by("direction", "strength")                          \
+        .order_by("direction", "strength")
+
+    return query
+
+# It's necessary to pass output of arithmetic on columns to
+# a function like cast, so it can be labeled.
+# Otherwise current version of sqlalchemy_utils_create_view raises an error.
--- src/db/views.py
+++ src/db/views.py
@@ -2,7 +2,7 @@ from sqlalchemy import MetaData
 from sqlalchemy_utils import create_view
 
 from db.schema import build_outpost_table
-from db.statistics import monthly_avg_rainfall, monthly_avg_temp
+from db.statistics import monthly_avg_rainfall, monthly_avg_temp, wind_rose
 
 
 def _create_outpost_stats_view(outpost_number,
@@ -11,7 +11,6 @@ def _create_outpost_stats_view(outpost_number,
                                refresh):
 
     view_name = f'o{outpost_number}_{stats_func.__name__}'
-
     metadata = MetaData()
     table = build_outpost_table(str(outpost_number))
     query = stats_func(table)
@@ -36,4 +35,11 @@ def create_view_monthly_avg_temp(outpost_number, engine, refresh=True):
 
     _create_outpost_stats_view(outpost_number, engine,
                                stats_func=monthly_avg_temp,
+                               refresh=refresh)
+
+
+def create_view_wind_rose(outpost_number, engine, refresh=True):
+
+    _create_outpost_stats_view(outpost_number, engine,
+                               stats_func=wind_rose,
                                refresh=refresh)
